30e6ec415586f0cb85d60c40b52a60a44d962c92
yeduy <59412389+yeduy@users.noreply.github.com>
2020-01-27 01:57:49 +0300

    Create Multiples-of-3-or-5-6kyu.js
    If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
    
    Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
    
    Note: If the number is a multiple of both 3 and 5, only count it once.

---
+++ Multiples-of-3-or-5-6kyu.js
@@ -0,0 +1,13 @@
+//My Code
+
+function solution(number){
+  var result = 0;  
+  for (i=0; i<number ; i++) {
+    if ( i % 3 === 0 || i % 5 === 0) {    
+      result += i;    
+      }
+   }  
+  return result; 
+}
+
+
